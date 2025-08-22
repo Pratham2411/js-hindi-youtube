763dc3e8138bd904ee6f37a6da4250c4e337c400
Pratham Raj <prathamraj2411@gmail.com>
2025-08-23 00:57:24 +0530

    Add files via upload

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,12 @@
+const accountId=144353
+let accountEmail="Pratham@google.com"
+var accountPassword="12345" // not used due to scope problems
+accountCity="Siwan"
+let accountstate;
+// accountId=2 not allowed
+console.log(accountId);
+accountEmail="abc@gmail.com"
+accountPassword="4321"
+accountCity="Patna"
+console.table([accountEmail,accountPassword,accountId,accountCity,accountstate])
+
---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,20 @@
+"use strict"; // treat all code as newer version of js
+// alert(3+3) // using node js not browser
+ let name ="Pratham"
+ let age=21
+ let loggedIn=false
+let state;
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => 
+// symbol => unique
+
+
+// object
+
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object
